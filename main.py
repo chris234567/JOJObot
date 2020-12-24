2e4dadb8fbbd6744c8aee239796a930ba9f766ef
Christoph Stade <christoph-stade@outlook.de>
2020-12-24 10:51:28 +0100

    added a random meme generator

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #bot driver file
 import discord
 import random
+import os
 
 client = discord.Client()
 
@@ -13,27 +14,43 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('-commands.txt'):
-        commands = open('commands.txt.txt', 'r', encoding='utf-8').readlines()
+    # list of commands
+    if message.content.startswith('-commands'):
+        commands = open('commands.txt', 'r', encoding='utf-8').readlines()
         for command in commands:
             await message.channel.send(command)
 
+    # inital greeting
     if message.content.startswith('-hello'):
         await message.channel.send('peep poop, im a bot:]')
 
+    # random short quote
     if message.content.startswith('-jojo light'):
-        quotes = open('quotes.txt', 'r', encoding='utf-8').readlines()
+        quotes = open('Media\quotes.txt', 'r', encoding='utf-8').readlines()
         i = random.randint(23, len(quotes) - 1)
         await message.channel.send(format(quotes[i].strip()))
 
+    # random long quote
     if message.content.startswith('-jojo elaborate'):
-        quotes = open('quotes.txt', 'r', encoding='utf-8').readlines()
+        quotes = open('Media\quotes.txt', 'r', encoding='utf-8').readlines()
         i = random.randint(1, 22)
         await message.channel.send(format(quotes[i].strip()))
 
-    if message.content.startswith('-zawarudo'):
-        await message.channel.send(file=discord.File('myGif.gif'))
+    # random meme
+    if message.content.startswith('-jojo meme'):
+        path = r"C:\Users\Chris\PycharmProjects\DiscordBot\Media\Memes"
+        random_filename = random.choice([
+            x for x in os.listdir(path)
+            if os.path.isfile(os.path.join(path, x))
+        ])
+        await message.channel.send(file=discord.File('Media\Memes\\' + random_filename))
 
-client.run('NzkxMzQwMTgzODE1NTg1Nzky.X-Nu-g.e5BwC2n_rKUFx-vRBi2f2V7CEpk')
+    if message.content.startswith('-zawarudo'):
+        await message.channel.send(file=discord.File('Media\myGif.gif'))
+
+
+token = open('token', 'r', encoding='utf-8').readline()
+
+client.run(token)
 
 
